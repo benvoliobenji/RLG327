e63b55d418ad72b917d47acc498d4a370ca50ab2
Benjamin Vogel <bavogel@iastate.edu>
2018-04-01 16:50:00 -0500

    More adjustments to object.h

--- object.h
+++ object.h
@@ -20,6 +20,62 @@ private:
 	int attribute;
 	int value;
 	dice damage;
+	bool artifact;
+	uint32_t rarity;
+	bool picked_up;
+	int xPos;
+	int yPos;
+
+public:
+	object(object_description obj, int xPos, int yPos);
+
+	~object();
+
+	string get_name();
+
+	string get_description();
+
+	object_type_t get_type();
+
+	uint32_t get_color();
+
+	int get_hp();
+
+	int get_dodge();
+
+	int get_defence();
+
+	int get_weight();
+
+	int get_speed();
+
+	int get_attribute();
+
+	int get_value();
+
+	int32_t get_damage_base();
+
+	uint32_t get_damage_number();
+
+	uint32_t get_damage_side();
+
+	bool get_artifact();
+
+	uint32_t get_rarity();
+
+	bool get_picked_up();
+
+	bool pick_up();
+
+	int get_xPos();
+
+	int get_yPos();
+
+	int set_xPos();
+
+	int set_yPos();
+
+	int roll_damage();
 };
 #endif
  
